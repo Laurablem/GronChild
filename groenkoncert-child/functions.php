257372a2275831e2c6c2a6c6c51547f0924f3393
Laurablem <eaa24lbvi@students.eaaa.dk>
2026-05-18 03:26:47 +0200

    tilføjer komentarer
    gør det overskuligt for mig selv med komentarer til

--- groenkoncert-child/functions.php
+++ groenkoncert-child/functions.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Enqueue parent theme styles in child theme.
- */
+
+/* Indlæser parent theme og child theme CSS gennem WordPress’ enqueue-system,
+så child theme CSS bliver indlæst efter parent theme CSS */
 function groenkoncert_child_enqueue_styles() {
 	wp_enqueue_style(
 		'groenkoncert-parent-style',
@@ -17,11 +17,12 @@ function groenkoncert_child_enqueue_styles() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
+
+/* Sørger for at WordPress kører styles-funktionen i enqueue-systemet */
 add_action( 'wp_enqueue_scripts', 'groenkoncert_child_enqueue_styles', 20 );
 
-/**
- * Enqueue child JavaScript after parent "main" script.
- */
+/* Indlæser child theme JavaScript gennem WordPress’ enqueue-system,
+så scriptet bliver indlæst efter jQuery og parent theme-scriptet */
 function groenkoncert_child_enqueue_scripts() {
 	wp_enqueue_script(
 		'groenkoncert-child-main',
@@ -31,4 +32,6 @@ function groenkoncert_child_enqueue_scripts() {
 		true
 	);
 }
+
+/* Sørger for at WordPress kører JavaScript-funktionen i enqueue-systemet */
 add_action( 'wp_enqueue_scripts', 'groenkoncert_child_enqueue_scripts', 30 );

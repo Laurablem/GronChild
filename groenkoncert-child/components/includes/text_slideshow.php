c301f202037ceb87efca2cd6f760859de23410bc
Laurablem <eaa24lbvi@students.eaaa.dk>
2026-05-18 04:05:14 +0200

    lavet kommentarer for overblik
    nye kommentarer

--- groenkoncert-child/components/includes/text_slideshow.php
+++ groenkoncert-child/components/includes/text_slideshow.php
@@ -1,13 +1,13 @@
 <?php
-    // Layout styrer om billede står til venstre eller højre på desktop.
+    /* Layout styrer om billede står til venstre eller højre på desktop */
     $layout = get_sub_field('layout');
     
-    // Henter tekstindhold og knapdata fra ACF.
+    /* Henter tekstindhold og knapdata */
     $title = get_sub_field('overskrift');
     $text = get_sub_field('tekst');
     $btn = get_sub_field('knap_button');
 
-    // Sikrer at slider-biblioteket er indlæst til galleriet.
+    /* Indlæser Flickity-biblioteket, så slideshowet kan fungere som en slider */
     wp_enqueue_script( 'flickity' );
 
 ?>
@@ -16,10 +16,10 @@
         <div class="row d-flex align-items-center">
             <div class="col-lg-6 <?php if($layout=="left"): echo 'order-lg-1';endif; ?>">
                 <div class="textimg__slideshow">
-                    <!-- Billedslider: vises kun hvis der er slideshow-rækker -->
+                    <!-- Billedslider til print-slideshowet -->
                     <?php if(have_rows('slideshow')): ?>
                         <div class="image-gallery-child">
-                            <!-- Looper billeder til slideshowet -->
+                            <!-- Looper gennem alle slideshow-billeder -->
                             <?php while(have_rows('slideshow')): the_row(); ?>
                                 <div class="textimg__slideshow-item">
                                     <img src="<?php echo get_sub_field('billede')['url']; ?>" alt="<?php echo get_sub_field('billede')['alt']; ?>">
@@ -31,7 +31,7 @@
             </div>
             <div class="col-lg-6 <?php if($layout=="left"): echo 'order-lg-0';endif; ?>">
                 <div class="textimg-wrap">
-                    <!-- Tekstindhold i højre/venstre kolonne afhængigt af layout -->
+                    <!-- Tekstindhold til print-slideshowet -->
                     <?php if($title): ?>
                         <h2 class="textimg__title"><?php echo $title; ?></h2>
                     <?php endif; ?>
@@ -39,7 +39,7 @@
                         <div class="textimg__text"><?php echo $text; ?></div>
                     <?php endif; ?>
                     <?php if( $btn['link']):?>
-                        <!-- CTA-knap med dynamisk link, target, farve og størrelse -->
+                        <!-- Mulighed for ekstra link-knap i modulet -->
                         <a href="<?php echo $btn['link']['url']; ?>" <?php echo !empty($btn['link']['target']) ? 'target="' . $btn['link']['target']. '"' : ''; ?>  class="btn btn-<?php echo $btn['farve']?> btn-<?php echo $btn['storrelse']; ?>"><span><?php echo $btn['link_label']; ?></span></a>
                     <?php endif; ?>
                 </div>
